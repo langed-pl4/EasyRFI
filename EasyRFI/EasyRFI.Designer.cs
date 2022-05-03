69d9da66c0027e8a88a85cde2d6689da0e257391
langed <diogo.lange@siemens.com>
2022-05-03 18:25:39 -0300

    Trocado a Listview pela DataGridView para conseguir fazer word wrap nas linhas da tabela

--- EasyRFI/EasyRFI.Designer.cs
+++ EasyRFI/EasyRFI.Designer.cs
@@ -34,11 +34,6 @@ namespace EasyRFI
             this.imgPerg = new System.Windows.Forms.ImageList(this.components);
             this.btnAddPergunta = new System.Windows.Forms.Button();
             this.btnAltPergunta = new System.Windows.Forms.Button();
-            this.lstPerguntas = new System.Windows.Forms.ListView();
-            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.btnImportarCSV = new System.Windows.Forms.Button();
             this.btnDelPergunta = new System.Windows.Forms.Button();
@@ -48,8 +43,20 @@ namespace EasyRFI
             this.colProduto = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colAderencia = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colJustificativa = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtPergunta = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cmbProduto = new System.Windows.Forms.ComboBox();
+            this.cmbAderencia = new System.Windows.Forms.ComboBox();
+            this.txtJustificativa = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnFiltrar = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dtPerguntas)).BeginInit();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // imgPerg
@@ -81,49 +88,9 @@ namespace EasyRFI
             this.btnAltPergunta.UseVisualStyleBackColor = true;
             this.btnAltPergunta.Click += new System.EventHandler(this.btnAltPergunta_Click);
             // 
-            // lstPerguntas
-            // 
-            this.lstPerguntas.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.columnHeader2,
-            this.columnHeader3,
-            this.columnHeader4,
-            this.columnHeader5});
-            this.lstPerguntas.FullRowSelect = true;
-            this.lstPerguntas.GridLines = true;
-            this.lstPerguntas.HideSelection = false;
-            this.lstPerguntas.Location = new System.Drawing.Point(0, 355);
-            this.lstPerguntas.Margin = new System.Windows.Forms.Padding(5);
-            this.lstPerguntas.Name = "lstPerguntas";
-            this.lstPerguntas.Size = new System.Drawing.Size(1372, 356);
-            this.lstPerguntas.SmallImageList = this.imgPerg;
-            this.lstPerguntas.TabIndex = 7;
-            this.lstPerguntas.UseCompatibleStateImageBehavior = false;
-            this.lstPerguntas.View = System.Windows.Forms.View.Details;
-            this.lstPerguntas.SelectedIndexChanged += new System.EventHandler(this.lstPerguntas_SelectedIndexChanged);
-            this.lstPerguntas.DoubleClick += new System.EventHandler(this.btnAltPergunta_Click);
-            // 
-            // columnHeader2
-            // 
-            this.columnHeader2.Text = "Pergunta";
-            this.columnHeader2.Width = 382;
-            // 
-            // columnHeader3
-            // 
-            this.columnHeader3.Text = "Produto";
-            this.columnHeader3.Width = 166;
-            // 
-            // columnHeader4
-            // 
-            this.columnHeader4.Text = "Aderência";
-            this.columnHeader4.Width = 192;
-            // 
-            // columnHeader5
-            // 
-            this.columnHeader5.Text = "Justificativa";
-            this.columnHeader5.Width = 526;
-            // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.groupBox2);
             this.groupBox1.Controls.Add(this.btnImportarCSV);
             this.groupBox1.Controls.Add(this.btnDelPergunta);
             this.groupBox1.Controls.Add(this.btnAddPergunta);
@@ -131,7 +98,7 @@ namespace EasyRFI
             this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
             this.groupBox1.Location = new System.Drawing.Point(0, 0);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(1408, 59);
+            this.groupBox1.Size = new System.Drawing.Size(1408, 129);
             this.groupBox1.TabIndex = 8;
             this.groupBox1.TabStop = false;
             // 
@@ -173,36 +140,155 @@ namespace EasyRFI
             this.colProduto,
             this.colAderencia,
             this.colJustificativa});
-            this.dtPerguntas.Location = new System.Drawing.Point(21, 99);
+            this.dtPerguntas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dtPerguntas.Location = new System.Drawing.Point(0, 129);
+            this.dtPerguntas.MultiSelect = false;
             this.dtPerguntas.Name = "dtPerguntas";
-            this.dtPerguntas.Size = new System.Drawing.Size(1161, 199);
+            this.dtPerguntas.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dtPerguntas.Size = new System.Drawing.Size(1408, 582);
             this.dtPerguntas.TabIndex = 10;
+            this.dtPerguntas.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtPerguntas_CellContentClick);
+            this.dtPerguntas.CellFormatting += new System.Windows.Forms.DataGridViewCellFormattingEventHandler(this.dtPerguntas_CellFormatting);
+            this.dtPerguntas.DoubleClick += new System.EventHandler(this.btnAltPergunta_Click);
             // 
             // colPergunta
             // 
+            this.colPergunta.FillWeight = 73.3595F;
             this.colPergunta.HeaderText = "Pergunta";
             this.colPergunta.Name = "colPergunta";
             this.colPergunta.Resizable = System.Windows.Forms.DataGridViewTriState.True;
-            this.colPergunta.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
-            this.colPergunta.Width = 300;
+            this.colPergunta.Width = 400;
             // 
             // colProduto
             // 
+            this.colProduto.FillWeight = 119.6079F;
             this.colProduto.HeaderText = "Produto";
             this.colProduto.Name = "colProduto";
-            this.colProduto.Width = 150;
+            this.colProduto.Width = 120;
             // 
             // colAderencia
             // 
+            this.colAderencia.FillWeight = 203.0457F;
             this.colAderencia.HeaderText = "Aderência";
             this.colAderencia.Name = "colAderencia";
-            this.colAderencia.Width = 200;
+            this.colAderencia.Width = 120;
             // 
             // colJustificativa
             // 
+            this.colJustificativa.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.colJustificativa.FillWeight = 3.986931F;
             this.colJustificativa.HeaderText = "Justificativa";
             this.colJustificativa.Name = "colJustificativa";
-            this.colJustificativa.Width = 300;
+            // 
+            // txtPergunta
+            // 
+            this.txtPergunta.Location = new System.Drawing.Point(113, 26);
+            this.txtPergunta.Name = "txtPergunta";
+            this.txtPergunta.Size = new System.Drawing.Size(133, 27);
+            this.txtPergunta.TabIndex = 10;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.btnClear);
+            this.groupBox2.Controls.Add(this.btnFiltrar);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.txtJustificativa);
+            this.groupBox2.Controls.Add(this.cmbAderencia);
+            this.groupBox2.Controls.Add(this.cmbProduto);
+            this.groupBox2.Controls.Add(this.txtPergunta);
+            this.groupBox2.Location = new System.Drawing.Point(12, 59);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(1384, 64);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Filtrar Por:";
+            // 
+            // cmbProduto
+            // 
+            this.cmbProduto.FormattingEnabled = true;
+            this.cmbProduto.Location = new System.Drawing.Point(350, 26);
+            this.cmbProduto.Name = "cmbProduto";
+            this.cmbProduto.Size = new System.Drawing.Size(126, 29);
+            this.cmbProduto.TabIndex = 14;
+            // 
+            // cmbAderencia
+            // 
+            this.cmbAderencia.FormattingEnabled = true;
+            this.cmbAderencia.Location = new System.Drawing.Point(606, 26);
+            this.cmbAderencia.Name = "cmbAderencia";
+            this.cmbAderencia.Size = new System.Drawing.Size(123, 29);
+            this.cmbAderencia.TabIndex = 15;
+            // 
+            // txtJustificativa
+            // 
+            this.txtJustificativa.Location = new System.Drawing.Point(874, 26);
+            this.txtJustificativa.Name = "txtJustificativa";
+            this.txtJustificativa.Size = new System.Drawing.Size(166, 27);
+            this.txtJustificativa.TabIndex = 16;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 21);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Pergunta:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(268, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 21);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Produto:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(503, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 21);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Aderência:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(760, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 21);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Justificativa:";
+            // 
+            // btnFiltrar
+            // 
+            this.btnFiltrar.Location = new System.Drawing.Point(1116, 19);
+            this.btnFiltrar.Margin = new System.Windows.Forms.Padding(5);
+            this.btnFiltrar.Name = "btnFiltrar";
+            this.btnFiltrar.Size = new System.Drawing.Size(125, 37);
+            this.btnFiltrar.TabIndex = 12;
+            this.btnFiltrar.Text = "&Filtrar";
+            this.btnFiltrar.UseVisualStyleBackColor = true;
+            this.btnFiltrar.Click += new System.EventHandler(this.btnFiltrar_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(1251, 19);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(5);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(125, 37);
+            this.btnClear.TabIndex = 21;
+            this.btnClear.Text = "&Limpar";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
             // 
             // EasyRFI
             // 
@@ -210,15 +296,16 @@ namespace EasyRFI
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1408, 733);
             this.Controls.Add(this.dtPerguntas);
-            this.Controls.Add(this.lstPerguntas);
-            this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.statusStrip1);
             this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(5);
             this.Name = "EasyRFI";
             this.Text = "EasyRFI";
             this.groupBox1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dtPerguntas)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -228,11 +315,6 @@ namespace EasyRFI
         private System.Windows.Forms.ImageList imgPerg;
         private System.Windows.Forms.Button btnAddPergunta;
         private System.Windows.Forms.Button btnAltPergunta;
-        private System.Windows.Forms.ListView lstPerguntas;
-        private System.Windows.Forms.ColumnHeader columnHeader2;
-        private System.Windows.Forms.ColumnHeader columnHeader3;
-        private System.Windows.Forms.ColumnHeader columnHeader4;
-        private System.Windows.Forms.ColumnHeader columnHeader5;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.Button btnDelPergunta;
@@ -242,6 +324,17 @@ namespace EasyRFI
         private System.Windows.Forms.DataGridViewTextBoxColumn colProduto;
         private System.Windows.Forms.DataGridViewTextBoxColumn colAderencia;
         private System.Windows.Forms.DataGridViewTextBoxColumn colJustificativa;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnFiltrar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtJustificativa;
+        private System.Windows.Forms.ComboBox cmbAderencia;
+        private System.Windows.Forms.ComboBox cmbProduto;
+        private System.Windows.Forms.TextBox txtPergunta;
+        private System.Windows.Forms.Button btnClear;
     }
 }
 
--- EasyRFI/EasyRFI.cs
+++ EasyRFI/EasyRFI.cs
@@ -18,6 +18,7 @@ namespace EasyRFI
         string jsonader = @"C:\Users\langed\Desenv\EasyRFI\DB\Aderencia.json";
 
         Perguntas perguntas = new Perguntas();
+        DataTable dt;
 
         public void RegistrarMensagemdeErro(object sender, EventArgs e)
         {
@@ -40,28 +41,39 @@ namespace EasyRFI
             if (System.IO.File.Exists(jsonperg))
                 this.perguntas = new Perguntas().Carregar(jsonperg);
 
-            //Vou popular uma linha para cada produto, dessa forma depois posso aplicar os filtros em cima da Listview direto
-            lstPerguntas.Items.Clear();
-            foreach (Pergunta perg in this.perguntas.perguntas)
-                    foreach(string prod in perg.regra.produtos)
-                    {
-                        ListViewItem it = lstPerguntas.Items.Add(perg.pergunta, 0);
-                        it.SubItems.Add(prod);
-                        it.SubItems.Add(perg.regra.aderencia);
-                        it.SubItems.Add(perg.regra.justificativa);
-                    }
+            //bookmark
+            int FirstDisplayedScrollingRowIndex = this.dtPerguntas.FirstDisplayedScrollingRowIndex; //Save Current Scroll Index
+            int SelectedRowIndex = 0;
+            if (this.dtPerguntas.SelectedRows.Count > 0) SelectedRowIndex = this.dtPerguntas.SelectedRows[0].Index; //Save Current Selected Row Index
+
+            //Vou popular uma linha para cada produto, dessa forma depois posso aplicar os filtros em cima da datagrid
+
+            dtPerguntas.Columns.Clear();
+            dtPerguntas.DefaultCellStyle.WrapMode = DataGridViewTriState.True;
+            dtPerguntas.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
 
 
-            dtPerguntas.Rows.Clear();
+            dt = new DataTable();
+
+            dt.Columns.Add("Pergunta", typeof(string));            
+            dt.Columns.Add("Produto", typeof(string));
+            dt.Columns.Add("Aderencia", typeof(string));
+            dt.Columns.Add("Justificativa", typeof(string));
+
             foreach (Pergunta perg in this.perguntas.perguntas)
                 foreach (string prod in perg.regra.produtos)
-                {
-                    dtPerguntas.Rows.Add(perg.pergunta, prod, perg.regra.aderencia, perg.regra.justificativa);
-                }
+                    dt.Rows.Add(new object[] { perg.pergunta, prod, perg.regra.aderencia, perg.regra.justificativa });
 
+            dtPerguntas.DataSource = dt;
 
+            dtPerguntas.Columns[0].Width = 400;
+            dtPerguntas.Columns[1].Width = 150;
+            dtPerguntas.Columns[2].Width = 150;
+            dtPerguntas.Columns[3].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
 
-
+            //bookmark
+            if ((FirstDisplayedScrollingRowIndex >= 0) && ((this.dtPerguntas.Rows.Count - 1) >= FirstDisplayedScrollingRowIndex)) this.dtPerguntas.FirstDisplayedScrollingRowIndex = FirstDisplayedScrollingRowIndex; //Restore Scroll Index
+            if ((this.dtPerguntas.Rows.Count - 1) >= SelectedRowIndex) this.dtPerguntas.Rows[SelectedRowIndex].Selected = true; //Restore Selected Row
         }
 
         private void NovaPergunta(string txtPergunta, List<string>lstProdutos, string txtAderencia, string txtJustificativa)
@@ -106,9 +118,7 @@ namespace EasyRFI
                     Pergunta perg = new Pergunta() { pergunta = txtPergunta, regra = reg };
                     this.perguntas.perguntas.Add(perg);
                     this.perguntas.Salvar(jsonperg);
-                }
-
-                this.CarregarDadosPerguntas();
+                }                
             }
             catch (Exception ex)
             {
@@ -142,63 +152,60 @@ namespace EasyRFI
                         lstProdutos.Add(f.txtPergunta.Text);
 
                     this.NovaPergunta(f.txtPergunta.Text, lstProdutos, f.cmbAderencia.Text, f.txtJustificativa.Text);
+                    this.CarregarDadosPerguntas();
                 }
             }
         }
 
         private void btnAltPergunta_Click(object sender, EventArgs e)
         {
-            if (lstPerguntas.SelectedItems.Count > 0)
+            if (dtPerguntas.CurrentRow.Index >= 0)
             {
                 //só quero a primeira
-                string pergunta = lstPerguntas.SelectedItems[0].SubItems[0].Text;
-                string justificativa = lstPerguntas.SelectedItems[0].SubItems[3].Text;
+                string pergunta = dtPerguntas.CurrentRow.Cells[0].Value.ToString();
+                string produto = dtPerguntas.CurrentRow.Cells[1].Value.ToString();
+                string justificativa = dtPerguntas.CurrentRow.Cells[3].Value.ToString();
 
-                Pergunta perg = this.perguntas.perguntas.Find(p => p.pergunta == pergunta && p.regra.justificativa == justificativa);
-                if ((perg != null) && (perg.regra.produtos.Count > 0) && (perg.regra.produtos.Contains(lstPerguntas.SelectedItems[0].SubItems[1].Text)))
-                {
-                    using (frmPergunta f = new frmPergunta(this.jsonprod,this.jsonperg, perg.regra.produtos, this.perguntas))
+                List<Pergunta> pergs = this.perguntas.perguntas.FindAll(p => p.pergunta == pergunta && p.regra.justificativa == justificativa);
+                foreach (Pergunta perg in pergs)
+                    if ((perg != null) && (perg.regra.produtos.Count > 0) && (perg.regra.produtos.Contains(produto)))
                     {
-                        f.txtPergunta.Text = perg.pergunta;
-
-                        //popular aderencia
-                        Aderencia ader = new Aderencia().Carregar(jsonader);
-                        f.cmbAderencia.Items.Clear();
-                        foreach (string aderencia in ader.aderencia)
-                            f.cmbAderencia.Items.Add(aderencia);
-
-                        f.cmbAderencia.Text = perg.regra.aderencia;
-                        f.txtJustificativa.Text = perg.regra.justificativa;
-                                
-                        if (DialogResult.OK == f.ShowDialog(this))
+                        using (frmPergunta f = new frmPergunta(this.jsonprod, this.jsonperg, perg.regra.produtos, this.perguntas))
                         {
-                            if (!ader.aderencia.Contains(f.cmbAderencia.Text))
+                            f.txtPergunta.Text = perg.pergunta;
+
+                            //popular aderencia
+                            Aderencia ader = new Aderencia().Carregar(jsonader);
+                            f.cmbAderencia.Items.Clear();
+                            foreach (string aderencia in ader.aderencia)
+                                f.cmbAderencia.Items.Add(aderencia);
+
+                            f.cmbAderencia.Text = perg.regra.aderencia;
+                            f.txtJustificativa.Text = perg.regra.justificativa;
+
+                            if (DialogResult.OK == f.ShowDialog(this))
                             {
-                                ader.aderencia.Add(f.cmbAderencia.Text);
-                                ader.Salvar(jsonader);
+                                if (!ader.aderencia.Contains(f.cmbAderencia.Text))
+                                {
+                                    ader.aderencia.Add(f.cmbAderencia.Text);
+                                    ader.Salvar(jsonader);
+                                }
+
+                                List<string> produtos = new List<string>();
+                                foreach (ListViewItem it in f.lstProdutos.CheckedItems)
+                                    produtos.Add(it.Text);
+
+                                perg.regra.produtos = produtos;
+                                perg.regra.aderencia = f.cmbAderencia.Text;
+                                perg.regra.justificativa = f.txtJustificativa.Text;
+                                perg.pergunta = f.txtPergunta.Text;
+
+                                this.perguntas.Salvar(jsonperg);
+                                this.CarregarDadosPerguntas();
                             }
-
-                            List<string> produtos = new List<string>();
-                            foreach (ListViewItem it in f.lstProdutos.CheckedItems)
-                                produtos.Add(it.Text);
-
-                            perg.regra.produtos = produtos;
-                            perg.regra.aderencia = f.cmbAderencia.Text;
-                            perg.regra.justificativa = f.txtJustificativa.Text;
-                            perg.pergunta = f.txtPergunta.Text;
-
-                            this.perguntas.Salvar(jsonperg);
-                            this.CarregarDadosPerguntas();
                         }
                     }
-                }
             }
-        }
-
-        private void lstPerguntas_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-            btnAltPergunta.Enabled = btnDelPergunta.Enabled = (lstPerguntas.SelectedItems.Count > 0);
         }
 
         private void btnImportarCSV_Click(object sender, EventArgs e)
@@ -248,11 +255,12 @@ namespace EasyRFI
                                 if (colAderencia == -1) throw new Exception("Não encontrei a coluna 'aderencia'");
                                 if (colJustificativa == -1) throw new Exception("Não encontrei a coluna 'justificativa'");
 
-                                if (!produtos.produtos.Contains(arrayline[colProduto]))
-                                {
-                                    produtos.produtos.Add(arrayline[colProduto]);
-                                    produtos.Salvar(this.jsonprod);
-                                }
+                                if (arrayline.Length > colProduto)
+                                    if (!produtos.produtos.Contains(arrayline[colProduto]))
+                                    {
+                                        produtos.produtos.Add(arrayline[colProduto]);
+                                        produtos.Salvar(this.jsonprod);
+                                    }
 
                                 if ((arrayline[colpergunta].Trim().Length > 0) &&
                                     (arrayline[colProduto].Trim().Length > 0))
@@ -264,8 +272,46 @@ namespace EasyRFI
                         linenr++;
                     }
                     sread.Close();
+
+                    this.CarregarDadosPerguntas();
                 }
             }
+        }
+
+        private void dtPerguntas_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            btnAltPergunta.Enabled = btnDelPergunta.Enabled = (dtPerguntas.CurrentRow.Index >= 0);
+        }
+
+        private void dtPerguntas_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            foreach (DataGridViewRow row in dtPerguntas.Rows)
+                if (row.Cells.Count > 2)
+                    if (row.Cells[2].Value != null) 
+                        if ((row.Cells[2].Value.ToString().ToLower().Contains("não")) ||
+                            ((row.Cells[2].Value.ToString().ToLower().Contains("nao"))))
+                            row.Cells[2].Style.BackColor = Color.LightCoral;
+        }
+
+        private void btnFiltrar_Click(object sender, EventArgs e)
+        {
+            string rowfilter = "";
+            rowfilter = "Pergunta LIKE '%" + txtPergunta.Text + "%' AND " +
+                        "Produto LIKE '%" + cmbProduto.Text + "%' AND " +
+                        "Aderencia LIKE '%" + cmbAderencia.Text + "%' AND " +
+                        "Justificativa LIKE '%" + txtJustificativa.Text + "%' ";
+
+            DataView dv = dt.DefaultView;
+            dv.RowFilter = rowfilter;
+            dtPerguntas.DataSource = dv;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtPergunta.Text = "";
+            cmbProduto.Text = "";
+            cmbAderencia.Text = "";
+            txtJustificativa.Text = "";
         }
     }
 
